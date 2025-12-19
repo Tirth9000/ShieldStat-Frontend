3bbeeae19be540f5d88e87e1b793bc3d47cf43a5
Tirth9000 <sharmatirth09012@gmail.com>
2025-12-19 16:13:55 +0530

    feat: add tracing and type definitions for Next.js routes
    - Introduced a new trace file to log performance metrics for hot reloading, bundler setup, and server start times.
    - Generated type definitions for App Router, including props for page and layout components, ensuring type safety for route parameters and search parameters.

---
+++ .next/dev/build/chunks/[turbopack-node]_transforms_postcss_ts_8b645c74._.js
@@ -0,0 +1,13 @@
+module.exports = [
+"[turbopack-node]/transforms/postcss.ts { CONFIG => \"[project]/Downloads/Web-Dev/Projects/Allianz Cloud/ShieldStat-Frontend/postcss.config.mjs [postcss] (ecmascript)\" } [postcss] (ecmascript, async loader)", ((__turbopack_context__) => {
+
+__turbopack_context__.v((parentImport) => {
+    return Promise.all([
+  "chunks/c13f0_e9c84513._.js",
+  "chunks/[root-of-the-server]__6c8c84fe._.js"
+].map((chunk) => __turbopack_context__.l(chunk))).then(() => {
+        return parentImport("[turbopack-node]/transforms/postcss.ts { CONFIG => \"[project]/Downloads/Web-Dev/Projects/Allianz Cloud/ShieldStat-Frontend/postcss.config.mjs [postcss] (ecmascript)\" } [postcss] (ecmascript)");
+    });
+});
+}),
+];
---
+++ .next/dev/static/chunks/723ff_Projects_Allianz Cloud_ShieldStat-Frontend_src_app_favicon_ico_mjs_157cf47a._.js
@@ -0,0 +1,7 @@
+(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/c13f0_next_dist_b9b63bab._.js"
+],
+    source: "dynamic"
+});
